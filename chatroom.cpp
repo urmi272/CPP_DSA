9aca0dcce5f53c7a793285ee0d390ef427f106f5
urmi272 <urmibarman272@gmail.com>
2025-09-08 23:42:24 +0530

    codeforces

---
+++ chatroom.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool checkHello(string s){
+    string target = "hello";
+    int i = 0;
+    for(char c : s){
+        if (c == target[i]){
+            i++;
+        }
+        if (i == target.size()){
+            return true;
+        }
+    }
+}
+int main(){
+    string s;
+    cin>>s;
+    if (checkHello(s)){
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
